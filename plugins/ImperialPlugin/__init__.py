8b2d8ba1f1d56b51662a86ba3f40042adac570f1
Victor Larchenko <victor99992@gmail.com>
2016-09-17 14:01:10 +0600

    Added metric to imperial convert

--- plugins/ImperialPlugin/__init__.py
+++ plugins/ImperialPlugin/__init__.py
@@ -15,7 +15,7 @@ def getMetaData():
             "author": "Jaime van Kessel",
             "version": "2.2",
             "api": 3,
-            "description": i18n_catalog.i18nc("Description of plugin", "Extension that allows to quickly convert models in inches to metric")
+            "description": i18n_catalog.i18nc("Description of plugin", "Extension that allows to quickly convert models in metric and imperial units")
         }
     }
 
