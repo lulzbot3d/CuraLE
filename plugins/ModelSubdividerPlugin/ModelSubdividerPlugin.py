daecf38006b0074fd4462e8d1e86a9de849f9c1d
Victor Larchenko <victor99992@gmail.com>
2017-05-11 16:06:11 +0600

    T581: added base subdivide method

--- plugins/ModelSubdividerPlugin/ModelSubdividerPlugin.py
+++ plugins/ModelSubdividerPlugin/ModelSubdividerPlugin.py
@@ -6,6 +6,11 @@ from UM.Extension import Extension
 from UM.Scene.Plane import Plane
 from UM.i18n import i18nCatalog
 from UM.Operations.AddSceneNodeOperation import AddSceneNodeOperation
+from UM.Scene.Selection import Selection
+from UM.Logger import Logger
+from UM.Scene.SceneNode import SceneNode
+from UM.Operations.GroupedOperation import GroupedOperation
+from UM.Operations.RemoveSceneNodeOperation import RemoveSceneNodeOperation
 
 i18n_catalog = i18nCatalog("ModelSubdividerPlugin")
 
@@ -20,7 +25,26 @@ class ModelSubdividerPlugin(Extension):
         plane = Plane()
         scene = Application.getInstance().getController().getScene()
         operation = AddSceneNodeOperation(plane, scene.getRoot())
-        Application.getInstance().getOperationStack().push(operation)
+        operation.push()
 
     def subdivide(self):
-        pass
+        if Selection.getCount() != 2:
+            Logger.log("w", i18n_catalog.i18n("Cannot subdivide: objects != 2"))
+            return
+        object1 = Selection.getSelectedObject(0)
+        object2 = Selection.getSelectedObject(1)
+        if type(object1) is SceneNode and type(object2) is Plane:
+            obj = object1
+            plane = object2
+        elif type(object2) is SceneNode and type(object1) is Plane:
+            obj = object2
+            plane = object1
+        else:
+            Logger.log("w", i18n_catalog.i18n("Cannot subdivide: object and plane need to be selected"))
+            return
+
+        operation = GroupedOperation()
+        operation.addOperation(RemoveSceneNodeOperation(plane))
+        # operation.addOperation(RemoveSceneNodeOperation(obj))
+        # operation.addOperation(AddSceneNodeOperation(obj, obj.getParent()))
+        operation.push()
