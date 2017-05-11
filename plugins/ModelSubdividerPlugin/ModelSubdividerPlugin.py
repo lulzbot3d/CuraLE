2533bc59ecb9c701f55d9bbdc9599019fb2f7bb3
Victor Larchenko <victor99992@gmail.com>
2017-05-11 15:50:57 +0600

    T581: Added creation of plane

--- plugins/ModelSubdividerPlugin/ModelSubdividerPlugin.py
+++ plugins/ModelSubdividerPlugin/ModelSubdividerPlugin.py
@@ -1,8 +1,11 @@
 # Copyright (c) 2017 Aleph Objects, Inc.
 # Cura is released under the terms of the AGPLv3 or higher.
 
+from UM.Application import Application
 from UM.Extension import Extension
+from UM.Scene.Plane import Plane
 from UM.i18n import i18nCatalog
+from UM.Operations.AddSceneNodeOperation import AddSceneNodeOperation
 
 i18n_catalog = i18nCatalog("ModelSubdividerPlugin")
 
@@ -14,7 +17,10 @@ class ModelSubdividerPlugin(Extension):
         self.addMenuItem(i18n_catalog.i18n("Subdivide mesh by plane"), self.subdivide)
 
     def createPlane(self):
-        pass
+        plane = Plane()
+        scene = Application.getInstance().getController().getScene()
+        operation = AddSceneNodeOperation(plane, scene.getRoot())
+        Application.getInstance().getOperationStack().push(operation)
 
     def subdivide(self):
         pass
