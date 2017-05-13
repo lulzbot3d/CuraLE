8de92d4cffb4e22736cf48859420109cd58518ac
Victor Larchenko <victor99992@gmail.com>
2017-05-13 13:41:31 +0600

    T581: added cross function

--- plugins/ModelSubdividerPlugin/ModelSubdividerPlugin.py
+++ plugins/ModelSubdividerPlugin/ModelSubdividerPlugin.py
@@ -11,6 +11,9 @@ from UM.Logger import Logger
 from UM.Scene.SceneNode import SceneNode
 from UM.Operations.GroupedOperation import GroupedOperation
 from UM.Operations.RemoveSceneNodeOperation import RemoveSceneNodeOperation
+from UM.Mesh.MeshBuilder import MeshBuilder
+import numpy
+import math
 
 i18n_catalog = i18nCatalog("ModelSubdividerPlugin")
 
@@ -43,8 +46,52 @@ class ModelSubdividerPlugin(Extension):
             Logger.log("w", i18n_catalog.i18n("Cannot subdivide: object and plane need to be selected"))
             return
 
-        operation = GroupedOperation()
-        operation.addOperation(RemoveSceneNodeOperation(plane))
-        # operation.addOperation(RemoveSceneNodeOperation(obj))
-        # operation.addOperation(AddSceneNodeOperation(obj, obj.getParent()))
-        operation.push()
+        result = self._subdivide(obj, plane)
+        if type(result) is tuple:
+            operation = GroupedOperation()
+            operation.addOperation(RemoveSceneNodeOperation(plane))
+            operation.addOperation(RemoveSceneNodeOperation(obj))
+            operation.addOperation(AddSceneNodeOperation(result[0], obj.getParent()))
+            if len(result) == 2:
+                operation.addOperation(AddSceneNodeOperation(result[1], obj.getParent()))
+            operation.push()
+        else:
+            Logger.log("w", i18n_catalog.i18n("Cannot subdivide"))
+
+    def _subdivide(self, mesh, plane):
+        plane_mesh_data = plane.getMeshData()
+        plane_vertices = plane_mesh_data.getVertices()
+        plane_face = [plane_vertices[0], plane_vertices[1], plane_vertices[2]]
+        builders = [MeshBuilder(), MeshBuilder()]
+        mesh_data = mesh.getMeshData()
+        vertices = mesh_data.getVertices()
+        indices = mesh_data.getIndices()
+        faces = []
+        for i in range(0, len(indices), 3):
+            faces.append([vertices[indices[i]], vertices[indices[i + 1]], vertices[indices[i + 2]]])
+        intersected_faces = []
+        for f in faces:
+            intersected = False
+            for i in range(3):
+                i2 = i + 1 if i < 2 else 0
+                segment = [f[i], f[i2]]
+                if self.cross(plane_face, segment):
+                    intersected = True
+                    break
+            if intersected:
+                intersected_faces.append(f)
+        print(intersected_faces)
+        return mesh,
+
+    def cross(self, plane_face, segment):
+        epsilon = 1e-4
+        n = numpy.cross(plane_face[1] - plane_face[0], plane_face[2] - plane_face[0])
+        v = plane_face[0] - segment[0]
+        d = numpy.inner(n, v)
+        w = segment[1] - segment[0]
+        e = numpy.inner(n, w)
+        if math.fabs(e) > epsilon:
+            o = segment[0] + w * d / e
+            if numpy.inner(segment[0] - o, segment[1] - o) <= 0:
+                return True
+        return False
