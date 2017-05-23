0838aa15fffd4ed6e4c9abba16c295bc41085e57
ics <Elena Filenko>
2017-05-22 17:15:19 -0700

    T881: Crash on single extruder fixed

--- plugins/SolidView/SolidView.py
+++ plugins/SolidView/SolidView.py
@@ -119,34 +119,42 @@ class SolidView(View):
                         #   1.0
                         #]
 
-                        if extruder_index == 0:
+                        if not multi_extrusion:
                             uniforms["diffuse_color"] = [
                                 shade_factor * 0.75,
                                 shade_factor * 0.84,
                                 shade_factor * 0.18,
                                 1.0
                             ]
-                        elif extruder_index == 1:
-                            uniforms["diffuse_color"] = [
-                                shade_factor * 0.44,
-                                shade_factor * 0.49,
-                                shade_factor * 0.09,
-                                1.0
-                            ]
-                        elif extruder_index == 2:
-                            uniforms["diffuse_color"] = [
-                                shade_factor * 0.84,
-                                shade_factor * 0.89,
-                                shade_factor * 0.47,
-                                1.0
-                            ]
                         else:
-                            uniforms["diffuse_color"] = [
-                                shade_factor * 0.84,
-                                shade_factor * 0.89,
-                                shade_factor * 0.45,
-                                1.0
-                            ]
+                            if extruder_index == 0:
+                                uniforms["diffuse_color"] = [
+                                    shade_factor * 0.75,
+                                    shade_factor * 0.84,
+                                    shade_factor * 0.18,
+                                    1.0
+                                ]
+                            elif extruder_index == 1:
+                                uniforms["diffuse_color"] = [
+                                    shade_factor * 0.44,
+                                    shade_factor * 0.49,
+                                    shade_factor * 0.09,
+                                    1.0
+                                ]
+                            elif extruder_index == 2:
+                                uniforms["diffuse_color"] = [
+                                    shade_factor * 0.84,
+                                    shade_factor * 0.89,
+                                    shade_factor * 0.47,
+                                    1.0
+                                ]
+                            else:
+                                uniforms["diffuse_color"] = [
+                                    shade_factor * 0.84,
+                                    shade_factor * 0.89,
+                                    shade_factor * 0.45,
+                                    1.0
+                                ]
 
                     except ValueError:
                         pass
