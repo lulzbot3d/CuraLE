3529edf780d9f762fb2572ca999dc6917f4661b9
Marko Bocevski <marko.bocevski@keitaro.com>
2017-06-01 11:21:49 +0200

    Proper executable called

--- run_cura2.sh
+++ run_cura2.sh
@@ -5,7 +5,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 if [ -e /opt/qt58/bin/qt58-env.sh ]; then
   source /opt/qt58/bin/qt58-env.sh
 elif [ -e /opt/qt56/bin/qt56-env.sh ]; then
-  source /opt/qt56/bin/qt56-env.sh 
+  source /opt/qt56/bin/qt56-env.sh
 fi
 
 # Set up custom python3 site packages related to Cura2
@@ -13,4 +13,4 @@ export PYTHONPATH=/opt/cura2/python3.5/site-packages
 
 # run Cura2
 
-$DIR/cura2_app.py
+$DIR/cura_app.py
