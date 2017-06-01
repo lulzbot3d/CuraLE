a2f15a8e492e112dd75572aa1254eda9e43d7725
Marko Bocevski <marko.bocevski@keitaro.com>
2017-06-01 11:07:48 +0200

    Fixed cmake config and run_cura2 shell script

--- run_cura2.sh
+++ run_cura2.sh
@@ -2,17 +2,11 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Set up Qt-related ENV variables
-QT_BASE_DIR=/opt/qt58
-export QTDIR=$QT_BASE_DIR
-export PATH=$QT_BASE_DIR/bin:$PATH
-
-if [[ $(uname -m) == "x86_64" ]]; then
-  export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/x86_64-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
-else
-  export LD_LIBRARY_PATH=$QT_BASE_DIR/lib/i386-linux-gnu:$QT_BASE_DIR/lib:$LD_LIBRARY_PATH
+if [ -e /opt/qt58/bin/qt58-env.sh ]; then
+  source /opt/qt58/bin/qt58-env.sh
+elif [ -e /opt/qt56/bin/qt56-env.sh ]; then
+  source /opt/qt56/bin/qt56-env.sh 
 fi
-
-export PKG_CONFIG_PATH=$QT_BASE_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 
 # Set up custom python3 site packages related to Cura2
 export PYTHONPATH=/opt/cura2/python3.5/site-packages
