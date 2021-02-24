d6a0b692d6911196f1d1e606972d584d1637d6a7
Robert Henderson <rhenderson@lulzbot.com>
2021-02-24 10:06:43 -0600

    update to machinename from Pro to ProXT as well as typo correction in USBPrinterOutputDeviceManager

--- plugins/USBPrinting/USBPrinterOutputDeviceManager.py
+++ plugins/USBPrinting/USBPrinterOutputDeviceManager.py
@@ -245,7 +245,7 @@ class USBPrinterOutputDeviceManager(QObject, OutputDevicePlugin, Extension):
             "lulzbot_gladiator_dingy_cutworm":      "Marlin_ProXT_HardenedSteelPlus_2.0.0.144.1_78264b7f.bin",
             "lulzbot_gladiator_banded_tiger":      "Marlin_ProXT_HardenedSteel_2.0.0.144.1_78264b7f.bin",
             "lulzbot_gladiator_lutefisk":      "Marlin_ProXT_M175_2.0.0.144.1_78264b7f.bin",
-            "lulzbot_gladiator_cecropia":      "Marlin_ProXT_SignleExtruderAeroV2_2.0.0.144.1_78264b7f.bin",
+            "lulzbot_gladiator_cecropia":      "Marlin_ProXT_SingleExtruderAeroV2_2.0.0.144.1_78264b7f.bin",
             "lulzbot_gladiator_achemon":      "Marlin_ProXT_SmallLayer_2.0.0.144.1_78264b7f.bin",
             
 
