92b9bfb9534c9b540405b9e9ededbbdccbe1f92c
sfjelstad <sfjelstad@lulzbot.com>
2023-01-31 12:13:46 -0600

    quick syntax correction

--- cura/Settings/MaterialSettingsVisibilityHandler.py
+++ cura/Settings/MaterialSettingsVisibilityHandler.py
@@ -11,11 +11,10 @@ class MaterialSettingsVisibilityHandler(UM.Settings.Models.SettingVisibilityHand
             "material_soften_temperature",
             "material_probe_temperature",
             "material_wipe_temperature",
-            "material_part_removal_temperature"
+            "material_part_removal_temperature",
             "default_material_print_temperature",
             "default_material_bed_temperature",
             "material_standby_temperature",
-            #"material_flow_temp_graph",
             "cool_fan_speed",
             "retraction_amount",
             "retraction_speed",
