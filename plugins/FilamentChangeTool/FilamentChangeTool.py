a61dec5df500f9a76898399b87d59851476eba2f
sfjelstad <sfjelstad@lulzbot.com>
2023-10-23 16:19:00 -0500

    Not much yet at all, have some questions

--- plugins/FilamentChangeTool/FilamentChangeTool.py
+++ plugins/FilamentChangeTool/FilamentChangeTool.py
@@ -18,3 +18,104 @@ class FilamentChangeTool(Tool):
     def event(self, event):
         return True
 
+    def getSettingDataString(self):
+        return """{
+            "name": "Filament Change",
+            "key": "FilamentChange",
+            "metadata": {},
+            "version": 2,
+            "settings":
+            {
+                "layer_number":
+                {
+                    "label": "Layer",
+                    "description": "At what layer should color change occur. This will be before the layer starts printing. Specify multiple color changes with a comma.",
+                    "unit": "",
+                    "type": "str",
+                    "default_value": "1"
+                },
+                "firmware_config":
+                {
+                    "label": "Use Firmware Configuration",
+                    "description": "Use the settings in your firmware, or customise the parameters of the filament change here.",
+                    "type": "bool",
+                    "default_value": false
+                },
+                "initial_retract":
+                {
+                    "label": "Initial Retraction",
+                    "description": "Initial filament retraction distance. The filament will be retracted with this amount before moving the nozzle away from the ongoing print.",
+                    "unit": "mm",
+                    "type": "float",
+                    "default_value": 30.0,
+                    "enabled": "not firmware_config"
+                },
+                "later_retract":
+                {
+                    "label": "Later Retraction Distance",
+                    "description": "Later filament retraction distance for removal. The filament will be retracted all the way out of the printer so that you can change the filament.",
+                    "unit": "mm",
+                    "type": "float",
+                    "default_value": 300.0,
+                    "enabled": "not firmware_config"
+                },
+                "x_position":
+                {
+                    "label": "X Position",
+                    "description": "Extruder X position. The print head will move here for filament change.",
+                    "unit": "mm",
+                    "type": "float",
+                    "default_value": 0,
+                    "enabled": "not firmware_config"
+                },
+                "y_position":
+                {
+                    "label": "Y Position",
+                    "description": "Extruder Y position. The print head will move here for filament change.",
+                    "unit": "mm",
+                    "type": "float",
+                    "default_value": 0,
+                    "enabled": "not firmware_config"
+                },
+                "z_position":
+                {
+                    "label": "Z Position (relative)",
+                    "description": "Extruder relative Z position. Move the print head up for filament change.",
+                    "unit": "mm",
+                    "type": "float",
+                    "default_value": 0,
+                    "minimum_value": 0
+                },
+                "retract_method":
+                {
+                    "label": "Retract method",
+                    "description": "The gcode variant to use for retract.",
+                    "type": "enum",
+                    "options": {"U": "Marlin (M600 U)", "L": "Reprap (M600 L)"},
+                    "default_value": "U",
+                    "value": "\\\"L\\\" if machine_gcode_flavor==\\\"RepRap (RepRap)\\\" else \\\"U\\\"",
+                    "enabled": "not firmware_config"
+                },
+                "machine_gcode_flavor":
+                {
+                    "label": "G-code flavor",
+                    "description": "The type of g-code to be generated. This setting is controlled by the script and will not be visible.",
+                    "type": "enum",
+                    "options":
+                    {
+                        "RepRap (Marlin/Sprinter)": "Marlin",
+                        "RepRap (Volumetric)": "Marlin (Volumetric)",
+                        "RepRap (RepRap)": "RepRap",
+                        "UltiGCode": "Ultimaker 2",
+                        "Griffin": "Griffin",
+                        "Makerbot": "Makerbot",
+                        "BFB": "Bits from Bytes",
+                        "MACH3": "Mach3",
+                        "Repetier": "Repetier"
+                    },
+                    "default_value": "RepRap (Marlin/Sprinter)",
+                    "enabled": "false"
+                }
+            }
+        }"""
+
--- plugins/FilamentChangeTool/FilamentChangeTool.qml
+++ plugins/FilamentChangeTool/FilamentChangeTool.qml
@@ -6,6 +6,7 @@ import QtQuick.Controls 1.2
 import QtQuick.Layouts 1.15
 
 import UM 1.1 as UM
+import Cura 1.0 as Cura
 
 Item {
     id: base
@@ -26,39 +27,10 @@ Item {
         columnSpacing: UM.Theme.getSize("default_margin").width
         rowSpacing: UM.Theme.getSize("default_margin").width
 
-        Label {
-            id: quantityLabel
-
-            Layout.column: 0
-            Layout.row: 0
-            height: UM.Theme.getSize("setting_control").height
-            text: "Quantity";
-        }
-
-        SpinBox {
-            id: quantityTextField
-
-            Layout.column: 1
-            Layout.row: 0
-
-            width: UM.Theme.getSize("setting_control").width;
-            height: UM.Theme.getSize("setting_control").height;
-
-            minimumValue: 1
-            maximumValue: 99
-        }
-
-        Button {
-            id: multiplyButton
-
-            Layout.columnSpan: 2
-            Layout.fillWidth: true
-            Layout.row: 1
-
-            text: catalog.i18nc("@action:button","Multiply")
-            property bool needBorder: true
-
-            onClicked: CuraActions.multiplySelection(quantityTextField.value)
+        Column {
+            Label {
+                text: "Filament Change"
+            }
         }
     }
 
